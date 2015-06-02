84ef61ca435e83a2b6388dd55bea461147912fac
Cate Uselton <cathode.use@gmail.com>
2015-06-02 16:09:50 -0700

    added rescursive note

--- five_problems.rb
+++ five_problems.rb
@@ -48,6 +48,7 @@ end
 # Problem 3
 # Write a function that computes the list of the first 100 Fibonacci numbers. By definition, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two. As an example, here are the first 10 Fibonnaci numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, and 34.
 
+# recursion version (slower than non recursive)
 def fib(n)
   answer = []
   return n if n<= 1
